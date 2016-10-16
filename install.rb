e028a0153f61c756bac79e5062686992b8a5f003
Paul Henrion <paul@godzilab-games.com>
2016-10-16 14:58:46 +0200

    Ask before executing a task; default yes with —yes option

--- install.rb
+++ install.rb
@@ -5,7 +5,7 @@ require 'optparse'
 
 class Main
     def initialize(args)
-        @options = {}
+        @options = { :yes => false }
 
         optparser = OptionParser.new do |opts|
             opts.on("-f", "--force", "overwrite exisiting files") do |pass|
@@ -19,6 +19,10 @@ class Main
             opts.on("-s", "--sync", "read preferences of current user and copy in `Configuration` folder") do |pass|
                 @options[:sync] = pass
             end
+
+            opts.on("-y", "--yes", "answer yes to everything") do |pass|
+                @options[:sync] = pass
+            end
         end
 
         begin
@@ -30,62 +34,84 @@ class Main
         end
 
         @configuration = [
-            { :conf => "Configuration/Preferences", :path => "~/Library/Preferences" },
-            { :conf => "Configuration/ST3", :path => "~/Library/Application Support/Sublime Text 3" },
-            { :conf => "Configuration/Terminal", :path => "~", :post_flight => lambda { |file| FileUtils.mv(file, File.join(File.dirname(file), ".#{File.basename(file)}")) if File.exist?(file) } },
-            { :conf => "Configuration/Xcode", :path => "~/Library/Developer/Xcode/UserData" },
-            { :conf => "Configuration/AndroidStudio", :path => "~/Library/Preferences/AndroidStudio1.5" },
+            { :description => "macOS preferences (eg: Finder, Dock, etc.)", :conf => "Configuration/Preferences", :path => "~/Library/Preferences" },
+            { :description => "Sublime Text 3 configuration", :conf => "Configuration/ST3", :path => "~/Library/Application Support/Sublime Text 3" },
+            { :description => "Terminal configuration", :conf => "Configuration/Terminal", :path => "~", :post_flight => lambda { |file| FileUtils.mv(file, File.join(File.dirname(file), ".#{File.basename(file)}")) if File.exist?(file) } },
+            { :description => "Xcode configuration", :conf => "Configuration/Xcode", :path => "~/Library/Developer/Xcode/UserData" },
+            { :description => "AndroidStudio configuration", :conf => "Configuration/AndroidStudio", :path => "~/Library/Preferences/AndroidStudio1.5" },
         ]
     end
     
+    def askToExecute(question, code)
+        if !@options[:yes]
+            puts "#{question}? [yN]"
+            a = gets.chomp.downcase
+            if a == "y" || a == "yes" || a == "yep"
+                yield code
+            end
+        else
+            yield code
+        end
+    end
+
     def run()
         @configuration.each do |conf|
-            Dir.glob(File.join(conf[:conf], "**/**")) do |file|
-                output = File.join(conf[:path], file[conf[:conf].length, file.length - conf[:conf].length])
+            question = "Install #{conf[:description]}"
+            if @options[:undo]
+                question = "Remove #{conf[:description]}"
+            elsif @options[:sync]
+                question = "Copy current system's \"#{conf[:description]}\" into `Configuration` folder}"
+            end
 
-                file = File.expand_path(file)
-                output = File.expand_path(output)
+            code = lambda { |conf|
+                Dir.glob(File.join(conf[:conf], "**/**")) do |file|
+                    output = File.join(conf[:path], file[conf[:conf].length, file.length - conf[:conf].length])
 
-                if @options[:undo]
-                    if File.file?(output)
-                        puts "Remove #{output}"
-                        FileUtils.rm_f(output)
-                    end
-                elsif @options[:sync]
-                    if File.file?(output)
-                        FileUtils.cp(output, file, { :verbose => true })
-                    end
-                else
-                    if File.directory?(output)
-                        puts "#{output} already exists"
-                        next
-                    end
+                    file = File.expand_path(file)
+                    output = File.expand_path(output)
 
-                    if File.file?(output)
-                        if @options[:force]
-                            File.unlink(output)
-                        else
+                    if @options[:undo]
+                        if File.file?(output)
+                            FileUtils.rm_f(output)
+                        end
+                    elsif @options[:sync]
+                        if File.file?(output)
+                            FileUtils.cp(output, file, { :verbose => true })
+                        end
+                    else
+                        if File.directory?(output)
                             puts "#{output} already exists"
                             next
                         end
-                    end
 
-                    if File.directory?(File.dirname(output)) == false
-                        FileUtils.mkdir_p(File.dirname(output))
-                    end
+                        if File.file?(output)
+                            if @options[:force]
+                                File.unlink(output)
+                            else
+                                puts "#{output} already exists"
+                                next
+                            end
+                        end
 
-                    FileUtils.cp_r(file, output, { :verbose => true })
+                        if File.directory?(File.dirname(output)) == false
+                            FileUtils.mkdir_p(File.dirname(output))
+                        end
 
-                    if conf[:post_flight] != nil
-                        conf[:post_flight].call(output)
+                        FileUtils.cp_r(file, output, { :verbose => true })
+
+                        if conf[:post_flight] != nil
+                            conf[:post_flight].call(output)
+                        end
                     end
                 end
-            end
+            }
+
+            askToExecute(question, code)
+
         end
     end
 
 end
-
 
 main = Main.new(ARGV)
 main.run()
